872335c095e6bd1b9bacdd88a4a113d5499b5228
GULABCHAND KUMAR GUPTA <gulabchandkumarg@gmail.com>
2023-10-11 20:26:00 +0530

    Time: 282 ms (30.47%), Space: 10.5 MB (59.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& arr, int target) {
+//         vector<int>v(2);
+//         int n = arr.size();
+//         for(int i=0;i<n;i++){
+//             bool flag=true;
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[i]+arr[j]==target){
+//                     v[0]=i;
+//                     v[1]=j;
+//                     flag=false;
+                  
+//                     break;
+//                 }
+//             }
+//               if(flag==false)break;
+//         }
+//         return v;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
         vector<int>v(2);
         int n = arr.size();
         for(int i=0;i<n;i++){
-            bool flag=true;
             for(int j=i+1;j<n;j++){
                 if(arr[i]+arr[j]==target){
                     v[0]=i;
                     v[1]=j;
-                    flag=false;
-                  
-                    break;
+                    return v;
+                    
                 }
             }
-              if(flag==false)break;
+             
         }
         return v;
     }
