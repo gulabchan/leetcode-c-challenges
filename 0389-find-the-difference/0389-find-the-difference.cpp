a7b8cfddf9e8fd4402f6923f997152ac664ce7cb
GULABCHAND KUMAR GUPTA <gulabchandkumarg@gmail.com>
2023-10-19 22:57:59 +0530

    Time: 3 ms (57.73%), Space: 7 MB (76.57%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    char findTheDifference(std::string s, std::string t) {
-        char result = 0;
-        for (char c : s + t) {
-            result ^= c;
-        }
-        return result;
+    char findTheDifference(string s, string t) {
+        int n = s.length();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i=0; i<n; i++) if(s[i]!=t[i]) return t[i];
+        return t[n];
     }
 };
